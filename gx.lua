4114da2cf63659f19da5e2ec415ca71662ccf663
gxosty <6321b5arsi@gmail.com>
2022-08-14 21:17:41 +0500

    gx Updated
    gx was updated to usable state.

--- gx.lua
+++ gx.lua
@@ -3,8 +3,80 @@ local gx = {
 	_allow = true,
 	_menus = {},
 	_back = "back",
-	_back_function = nil
+	_back_function = nil,
+	vars = {},
+	signs = {[false] = "[-]", [true] = "[+]"}
 }
+
+-- [[ Menu related functions (I guess) ]] --
+
+-- Stackoverflow is <3
+function gx.copy_table(from, to_table)
+	-- erase all old keys
+	for k,_ in pairs(to_table) do
+		to_table[k] = nil
+	end
+
+	-- copy the new ones over
+	for k,v in pairs(from) do
+		if type(v) == "table" then
+			local _v = {}
+			gx.copy_table(v, _v)
+			to_table[k] = _v
+		else
+			to_table[k] = v
+		end
+	end
+end
+
+function gx.indexof(a, tbl)
+	for _,a_ in ipairs(tbl) do if a_==a then return true end end
+	return false
+end
+
+function gx.table_difference(a, b)
+	local ret = {}
+	for _,a_ in ipairs(a) do
+	   if not gx.indexof(a_,b) then table.insert(ret, a_) end
+	end
+	
+	return ret
+end
+
+function gx.table_merge(...)
+	local result = {}
+	-- For each source table
+	for _, t in ipairs{...} do
+		-- For each pair in t
+		for k, v in pairs(t) do
+			if result[k] == nil then
+				result[k] = v
+			else
+				table.insert(result, v)
+			end
+		end
+	end
+	return result
+end
+
+function gx.split_string(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+function gx.sign(b)
+	if b == true then
+		return gx.signs[true]
+	else
+		return gx.signs[false]
+	end
+end
 
 function gx.split_menu(menu)
 	local ml = {}
@@ -23,8 +95,150 @@ function gx.split_menu(menu)
 	return {menu = ml, functions = fl}
 end
 
+function gx.make_ib(index, len, bools)
+	local _index = {} 
+	local _bools = {}
+
+	for i = 1, len do
+		if bools[i] == true then
+			_bools[i] = true
+		else
+			_bools[i] = false
+		end
+	end
+
+	_bools[index] = not _bools[index]
+	_index = {index}
+
+	return {index = _index, bools = _bools}
+end
+
+function gx.split_index(index, len)
+	local _index = {}
+	local _bools = {}
+
+	for i = 1, len do
+		if index[i] == true then
+			table.insert(_index, i)
+			_bools[i] = true
+		else
+			_bools[i] = false
+		end
+	end
+
+	return {index = _index, bools = _bools}
+end
+
+function gx.index_diff(old, new)
+	local index = {}
+
+	index = gx.table_merge(gx.table_difference(old, new), gx.table_difference(new, old))
+
+	return index
+end
+
+function gx.switch_bools(index, bools)
+	for k, i in ipairs(index) do
+		bools[i] = not bools[i]
+	end
+
+	return bools
+end
+
+function gx.get_var(var_path)
+	if type(var_path) == "string" then
+		var_path = gx.split_string(var_path, ".")
+	end
+
+	local var = gx.vars
+
+	for k, v in ipairs(var_path) do
+		var = var[v]
+	end
+
+	return var
+end
+
+function gx.render_menu(menu, bools)
+	local _menu = {}
+	-- gg.toast(tostring(gx.vars["wb"]))
+
+	for k, v in ipairs(menu) do
+		local _v = tostring(v)
+		local fe = 1
+		while true do
+			local _s = _v:find("{gx:")
+			local _e = _v:find("}", fe)
+	
+			if _s ~= nil and _e ~= nil then
+				if _e - _s > 0 then
+					local name = _v:sub(_s, _e)
+					local var_path = name:sub(name:find(":") + 1, name:find("}") - 1)
+					local var = gx.get_var(var_path)
+					if type(var) == "boolean" then
+						var = gx.sign(var)
+					end
+					-- gg.alert(name, "ok")
+					_v = _v:gsub(name, tostring(var))
+				else
+					fe = _e + 1
+				end
+			else
+				break
+			end
+		end
+		_v = _v:gsub("{gxindex}", tostring(k))
+		_v = _v:gsub("{gxsign}", gx.sign(bools[k]))
+		table.insert(_menu, _v)
+	end
+
+	return _menu
+end
+
+function gx.format_args(args, ind, bool)
+	for i = 1, #args do
+		if type(args[i]) == "string" then
+			if args[i] == "{gxindex}" then
+				args[i] = ind
+			elseif args[i] == "{gxbool}" then
+				args[i] = bool
+			else
+				local _v = tostring(args[i])
+				local fe = 1
+				while true do
+					local _s = _v:find("{gx:")
+					local _e = _v:find("}", fe)
+	
+					if _s ~= nil and _e ~= nil then
+						if _e - _s > 0 then
+							local name = _v:sub(_s, _e)
+							local var_path = name:sub(name:find(":") + 1, name:find("}") - 1)
+							local var = gx.get_var(var_path)
+							_v = _v:gsub(name, tostring(var))
+						else
+							fe = _e + 1
+						end
+					else
+						break
+					end
+				end
+			end
+		end
+	end
+
+	return args
+end
+
 function gx.add_to_nav(t)
 	gx._nav[#gx._nav + 1] = t
+end
+
+function gx.set_back_text(text)
+	gx._back = text
+end
+
+function gx.set_signs(t)
+	gx.signs = t
 end
 
 function gx.add_menu(menu)
@@ -45,14 +259,32 @@ function gx.add_menu(menu)
 		gx._nav[1] = {name = menu.name, allow_stay = true}
 	end
 
-	local list = gx.split_menu(menu.menu)
-	menu.menu = list.menu
-	menu.functions = list.functions
-
-	if menu.type == "back" then
-		table.insert(menu.menu, gx._back)
-		menu.functions[#menu.functions + 1] = {gx._back_function}
+	if menu.type == "choice" or menu.type == "back" then
+		if type(menu.menu[1]) == "function" then
+			if #menu.menu == 1 then
+				menu["use_menu_function"] = true
+				menu["use_single_function"] = true
+			else
+				gg.toast("Menu \""..menu.name.."\" has invalid menu property")
+			end
+		else
+			local list = gx.split_menu(menu.menu)
+			menu.menu = list.menu
+			menu.functions = list.functions
+		end
+	elseif menu.type == "multi_choice" or menu.type == "multi_choice_s" then
+		if menu.use_single_function ~= true then
+			local list = gx.split_menu(menu.menu)
+			menu.menu = list.menu
+			menu.functions = list.functions
+		end
 	end
+
+	if menu.f == nil then
+		menu.f = {}
+	end
+	menu.bools = {}
+	menu.old_ind = {}
 
 	gx._menus[menu.name] = menu
 end
@@ -64,31 +296,155 @@ end
 
 gx._back_function = gx.go_back
 
+function gx.process_title(title)
+	_title = ""
+	if type(title) == "table" then
+		for k, v in ipairs(title) do
+			if type(v) == "table" then
+				if #v == 1 then
+					_title = _title..v[1]()
+				else
+					_title = _title..v[1](table.unpack(gx.format_args(v[2], nil, nil)))
+				end
+			else
+				_title = _title..v
+			end
+		end
+	else
+		_title = title
+	end
+
+	return _title
+end
+
+function gx.generate_menu(f)
+	if #f == 1 then
+		return f[1]()
+	else
+		return f[1](table.unpack(f[2]))
+	end
+end
+
+function gx.process_pre_function(f)
+	if f == nil then return end
+	if type(f) ~= "table" then 
+		gg.toast("\"pre_f\" is not a table")
+		return
+	end
+
+	if #f > 0 then
+		if #f == 1 then
+			f[1]()
+		else
+			f[1](table.unpack(gx.format_args(f[2], nil, nil)))
+		end
+	end
+end
+
+function gx.process_functions(functions, ind, bool)
+	if type(ind) == "number" then
+		ind = {ind}
+	end
+
+	if bool == nil then
+		bool = {}
+	end
+
+	for k, i in ipairs(ind) do
+		if #functions[i] > 0 then
+			if #functions[i] == 1 then
+				functions[i][1]()
+			else
+				functions[i][1](table.unpack(gx.format_args(functions[i][2], i, bool[i])))
+			end
+		end
+	end
+end
+
+function gx.process_single_function(f, ind, bool)
+	if bool == nil then
+		bool = {}
+	end
+
+	if #f > 0 then
+		if #f == 1 then
+			f[1]()
+		else
+			f[1](table.unpack(gx.format_args(f[2], ind, bool)))
+		end
+	end
+end
+
 function gx.open_menu(menu_name)
-	local the_menu = gx._menus[menu_name]
-	if the_menu == nil then
+	if gx._menus[menu_name] == nil then
 		gg.toast("Menu \""..menu_name.."\" doesn't exist.")
 		return
 	end
+	the_menu = {}
+	gx.copy_table(gx._menus[menu_name], the_menu)
 	local allow_stay = false
+	gx.process_pre_function(the_menu.pre_f)
+	local _title = gx.process_title(the_menu.title)
 
 	if the_menu.type == "back" then
 		allow_stay = true
 	else
 		allow_stay = false
 	end
+
 	if gx._nav[#gx._nav].name ~= menu_name then
 		gx.add_to_nav({name = the_menu.name, allow_stay = allow_stay})
 	end
-	local ch = gg.choice(the_menu.menu, nil, the_menu.title)
 
-	if ch == nil then return end
-	if #the_menu.functions[ch] > 0 then
-		if #the_menu.functions[ch] == 1 then
-			the_menu.functions[ch][1]()
-		else
-			the_menu.functions[ch][1](table.unpack(the_menu.functions[ch][2]))
+	if the_menu.use_menu_function == true then
+		the_menu.menu = gx.generate_menu(the_menu.menu)
+	end
+
+	local ind = nil
+	local _ib = nil
+
+	if the_menu.type == "back" or the_menu.type == "choice" then
+		if the_menu.type == "back" then
+			table.insert(the_menu.menu, gx._back)
 		end
+		local _m = gx.render_menu(the_menu.menu, the_menu.bools)
+		ind = gg.choice(_m, nil, _title)
+		if ind == nil then return end
+		if the_menu.type == "back" then
+			table.remove(_m)
+
+			if ind == #the_menu.menu then
+				gx._back_function()
+				return
+			end
+		end
+		_ib = gx.make_ib(ind, #_m, gx._menus[menu_name].bools)
+	elseif the_menu.type == "multi_choice" or the_menu.type == "multi_choice_s" then
+		local _m = gx.render_menu(the_menu.menu, the_menu.bools)
+		local _b = nil
+		if the_menu.type == "multi_choice_s" then _b = the_menu.bools end
+		ind = gg.multiChoice(_m, _b, _title)
+		if ind == nil then return end
+		_ib = gx.split_index(ind, #_m)
+		if the_menu.type == "multi_choice_s" then
+			gx._menus[menu_name].old_ind = _ib.index
+			_ib.index = gx.index_diff(the_menu.old_ind, _ib.index)
+			if #_ib.index == 0 then return end
+		elseif the_menu.type == "multi_choice" then
+			_ib.bools = gx.switch_bools(_ib.index, the_menu.bools)
+		end
+	else
+		gg.toast("Non existing menu type: "..the_menu.type..". Exiting...")
+		os.exit()
+	end
+
+	ind = _ib.index
+	gx._menus[menu_name].bools = _ib.bools
+		
+	if the_menu.use_single_function == true then
+		gx.process_single_function(the_menu.f, ind, gx._menus[menu_name].bools)
+	else
+		gx.process_functions(the_menu.functions, ind, gx._menus[menu_name].bools)
 	end
 end
 
@@ -117,6 +473,109 @@ function gx.start()
 
 	gx.nav_update()
 	gx.continue()
+end
+
+-- [[ Editor functions ]] --
+
+gx.editor = {
+	types = {
+		D = gg.TYPE_DWORD,
+		Q = gg.TYPE_QWORD,
+		F = gg.TYPE_FLOAT,
+		B = gg.TYPE_BYTE
+	}
+}
+
+gx.editor.set = function(values)
+	for k, v in ipairs(values) do
+		if type(v.flags) == "string" then
+			v.flags = gx.editor.types[v.flags]
+		end
+
+		if v.freeze == true then
+			gg.addListItems({v})
+		else
+			gg.removeListItems({v})
+		end
+	end
+
+	gg.setValues(values)
+end
+
+gx.editor.get = function(data)
+	for k, v in ipairs(data) do
+		if type(v.flags) == "string" then
+			v.flags = gx.editor.types[v.flags]
+		end
+	end
+
+	return gg.getValues(data)
+end
+
+gx.editor.set_string = function(data)
+	local values = {}
+
+	for k, v in ipairs(data) do
+		local _address = data[1]
+		local _bytes = gg.bytes(v[2])
+		local _len = data[3]
+	
+		if #_bytes < _len then
+			local _len2 = _len - #_bytes
+			for i = 1, _len2 do
+				table.insert(bytes,0)
+			end
+		end
+	
+		for i = 1, _len do
+			table.insert(values, {address = _address + (i - 1), flags = gg.TYPE_BYTE, value = bytes[i]})
+		end
+	end
+
+	gg.setValues(values)
+end
+
+gx.editor.get_string = function(data)
+	local values = {}
+
+	for k, v in ipairs(data) do
+		local _string = ""
+		local _address = v[1]
+		local _len = v[2]
+
+		for i = 0, amount do
+			_byte = gx.editor.get({{address = _address + i, flags = 'B'}})[1]
+	
+			if _byte < 1 then break end
+	
+			_string = _string .. string.char(_byte)
+		end
+		table.insert(values, _string)
+	end
+
+	return values
+end
+
+gx.editor.switch = function(data)
+	for k, v in ipairs(data) do
+		if v.bool == true then
+			if type(v.value) == "table" then
+				v.value = v.value[2]
+			end
+			if type(v.freeze) == "table" then
+				v.freeze = v.freeze[2]
+			end
+		else
+			if type(v.value) == "table" then
+				v.value = v.value[1]
+			end
+			if type(v.freeze) == "table" then
+				v.freeze = v.freeze[1]
+			end
+		end
+	end
+
+	gx.editor.set(data)
 end
 
 return gx
--- gx_test.lua
+++ gx_test.lua
@@ -1,35 +1,193 @@
-local response = gg.makeRequest("")
-local gx = load_string(response.text)()
+local gx = nil
+
+-- if gg.alert("Net?", "yes", "no") == 1 then
+if true then
+	local response = gg.makeRequest("http://192.168.1.100:9999/gx.lua")
+	gx = load(response.content)()
+else
+	gx = require("gx")
+end
+
+function get_map()
+	return "Eden"
+end
+
+function no_wind(index)
+	gg.toast(tostring(index))
+end
+
+propsid = {
+	{57978849,       "Windmill🏅"},
+	{-677716449,     "Purple Umbrella ☂️"},
+	{-644161211,     "Transparent Umbrella ☂️"},
+	{992885953,      "Table 🪑"},
+	{-1444947170,    "XMas table 🪑"},
+	{3580839943,     "Swing ❤"},
+	{3779226149,     "Campfire 🔥"},
+	{-1030495085,    "Seesaw 🔨"},
+	{3634028466,     "Flower Umbrella🌂"},
+	{2574095143,     "Umbrella ☔"},
+	{3269660804,     "Guitar 🎻"},
+	{2352004821,     "Ukulele 🎸"},
+	{-1382855507,    "Pipa 🎸"},
+	{396560731,      "Flute 🎶"},
+	{1237767078,     "Pan flute 🎺"},
+	{1889256860,     "Piano 🎹"},
+	{3275797515,     "White Piano 🎼"},
+	{3280753494,     "Firework 🎆"},
+	{4246711693,     "Shaker 🔔"},
+	{4196378836,     "Daser"},
+	{900914909,      "Steel Drum 🥁"},
+	{3499881140,     "Big Quilt Drum 🥁"},
+	{2003040587,     "Small Double Drum 🥁"},
+	{2671000446,     "Harp"},
+	{399957901,      "Xiloph"},
+	{1064146256,     "Circle Harp"},
+	{1021011859,     "Horn 📯"},
+	{2186944737,     "Ancestor Jar "},
+	{2041015009,     "Ancestral Kettle"},
+	{776794517,      "Ancestral Spear 🔱"},
+	{-896102798,     "Cornet 🎷"},
+	{1079120316,     "Bookcase 📚"},
+	{1994487329,     "Hammock 🛌"},
+	{-1762117817,    "Torch 🔥"},
+	{-1513173800,    "Tent ⛺"},
+	{-2094712299,    "lantern? 🏮"},
+	{1661108877,     "Hoop 🏀"},
+	{3314486409,     "Tea table 🍵"},
+	{351343999,      'Rose 🌹'},
+	{638976622,      'Star lamp ⭐'},
+	{-1723880395,    'Fox 🦊'},
+	{-777390487,     'Electro guitar 🎸'},
+	{-994414187,     'Birthday flag 🎉'},
+	{1638144370,     'Ocarina 🎵'},
+	{-2058340788,    'Yellow Umbrella ☂️'},
+	{1480625729,     'Double Chair 🪑🪑'},
+	{-1352265746,    'Wooden Double chair 🪑🪑'},
+	{1192794220,     'Pipe'},
+	{9427151,        'Beach ball 🏐'},
+	{-1192675672,    'Beach bed 🛏️'},
+	{1793801900,     'Stone fire 🔥'},
+	{313507026,      'Mini stone fire 🔥'},
+	{90660037,       "Aviary Flag 🏳️‍"},
+	{3772092866,     "Nightbook 🔹"},
+	{3454884039,     "Light Ball 🏐"},
+	{946432484,      "Island Umbrella ⛱️"}, 
+	{660764419,      "Brazier 3 🔥"},
+	{2546683657,     "Pumpkin 🎃"},
+	{2657110838,     "Kalimba 🎹"},
+	{1279207725,     "Mischief Table 🎃🪑"},
+	{-481291981,     "Spell Water 🌊"},
+	{1713655968,     "Spell Earth ⛰️"},
+	{-283779536,     "Spell Wind 💨"},
+	{2026179880,     "Spell Fire 🔥"},
+	{1931354705,     "Snow Globe 🔮"},
+	{-699266735,     "Pillow Xmas"},
+	{2035109393,     "Nothing"},
+	{0,              "Disable ❌"}
+}
+
+function prop_menu()
+	local _menu = {}
+
+	for k, v in ipairs(propsid) do
+		table.insert(_menu, "{gxindex}. "..v[2])
+	end
+
+	return _menu
+end
+
+function ftest(index)
+	gg.toast(tostring(index[1]))
+end
+
+function test2(index, bools)
+	local s = ""
+	for k, v in ipairs(index) do
+		s = s.."\n"..tostring(v).." = "..tostring(bools[v])
+	end
+	gg.toast(s)
+end
 
 gx.add_menu({
-	title = "Test Main Menu",
+	title = "Choose the hack:",
 	name = "main",
 	menu = {
-		{"Test1", {gg.open_menu, {"test1_menu"}}},
-		{"Test2", {gg.open_menu, {"test2_menu"}}},
+		{"[⬆️] Wall Breach: {gx:settings.wb}", nil},
+		{"[⏭] Farm", {gx.open_menu, {"farm"}}},
+		{"[🌀] Teleporter", {gx.open_menu, {"teleporter"}}},
+		{"[🪑] Prop Hack", {gx.open_menu, {"propmenu"}}},
+		{"[💻] Open (UI)", nil},
+		{"[💫] Spells", nil},
+		{"[🎉] Fun!", nil},
+		{"[🦋] Wings", nil},
+		{"[💨] No Wind Wall", {gg.toast, {"No Wind", true}}},
+		{"[✨] Other Hacks", {gx.open_menu, {"hackmenu"}}},
+		{"[⚙️] Settings", nil}
 	},
-	type = "normal"
+	type = "choice"
 })
 
 gx.add_menu({
-	title = "Test1 Menu",
-	name = "test1_menu",
+	title = {"Current map: ", {get_map}},
+	name = "farm",
 	menu = {
-		{"Hello", nil},
-		{"World", nil},
-	},
-	type = "normal"
-})
-
-gx.add_menu({
-	title = "Test2 Menu",
-	name = "test2_menu",
-	menu = {
-		{"Hello2", nil},
-		{"World2", nil},
+		{"{gxsign} [▶️] Semi-AutoCR", nil},
+		{"{gxsign} [▶️] Semi-WingLight Run", nil},
+		{"{gxsign} [📍] Teleport WLs to yourself", nil},
+		{"{gxsign} [📍] Teleport Statues to yourself⚠️", nil},
+		{"{gxsign} [☀️] Collect Waxes", nil},
+		{"{gxsign} [⭐] Collect WLs", nil},
+		{"{gxsign} [🔓] Unlock Elders", nil}
 	},
 	type = "back"
 })
+
+gx.add_menu({
+	title = {"Current map: ", {get_map}},
+	name = "teleporter",
+	menu = {
+		{"[⏩] Change Map", nil},
+		{"[🚩] Go to", nil}
+	},
+	type = "back"
+})
+
+gx.add_menu({
+	title = "Choose prop:",
+	name = "propmenu",
+	menu = {prop_menu},
+	f = {ftest, {"{gxindex}"}},
+	type = "choice"
+})
+
+gx.add_menu({
+	title = "Select hack:",
+	name = "hackmenu",
+	menu = {
+		"{gxsign} Autoburn 🔥",
+		"{gxsign} Unlock All Cosmetics & Emotes 🔓",
+		"{gxsign} Unlock Friendship Nodes 🔓",
+		"{gxsign} Read Chats",
+		"{gxsign} Unlimited Energy ♾️",
+		"{gxsign} Quick Steps ⚡",
+		"{gxsign} Remove Clouds ☁️",
+		"{gxsign} No Knockdown 🚹",
+		"{gxsign} God Mode"
+	},
+	use_single_function = true,
+	f = {test2, {"{gxindex}", "{gxbool}"}},
+	type = "multi_choice"
+})
+
+-- on  = '¦✅¦'
+-- off = '¦❌¦'
+
+gx.vars["settings"] = {
+	wb = 5.0
+}
+gx.set_back_text("[⬅️] Back")
+gx.set_signs({[false] = "¦❌¦", [true] = "¦✅¦"})
 
 while true do
 	if gg.isVisible(true) then
