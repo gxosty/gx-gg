9611f3a6c51f32f48ebbe579834c3cb678eec8bc
gxost <93872978+gxosty@users.noreply.github.com>
2022-08-21 00:09:37 +0500

    my bad fixed

--- gx.lua
+++ gx.lua
@@ -1,7 +1,7 @@
 --local response = gg.makeRequest("http://192.168.1.107:9999/json.lua")
 local response = gg.makeRequest("https://raw.githubusercontent.com/gxosty/gx-gg/main/json.lua")
---local json = load(response.content)()
-local json = require("gx.json")
+local json = load(response.content)()
+--local json = require("gx.json")
 
 local gx = {
 	_nav = nil,
